4a9e6a3b89661bf4fde8cbd3e5c1efa84f77f111
weszzzz <142858561+weszzzz@users.noreply.github.com>
2024-11-17 15:42:00 +0800

    1

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.wesz.fun": dockerHub,
+  "quay.wesz.fun": "https://quay.io",
+  "gcr.wesz.fun": "https://gcr.io",
+  "k8s-gcr.wesz.fun": "https://k8s.gcr.io",
+  "k8s.wesz.fun": "https://registry.k8s.io",
+  "ghcr.wesz.fun": "https://ghcr.io",
+  "cloudsmith.wesz.fun": "https://docker.cloudsmith.io",
+  "ecr.wesz.fun": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.wesz.fun": dockerHub
 };
 
 function routeByHosts(host) {
